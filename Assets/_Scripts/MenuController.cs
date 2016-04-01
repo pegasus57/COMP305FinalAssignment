348f97c14dbe132f241fe165403cb12fb324cf2e
JihoYoo <davidyoo214@gmail.com>
2016-03-31 23:58:37 -0400

    test

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Event Handler for StartButtonClick
+
+    public void StartButtonClick() 
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void InstructionsButtonClick()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void QuitButtonClick()
+    {
+        Application.Quit();
+    }
+}
