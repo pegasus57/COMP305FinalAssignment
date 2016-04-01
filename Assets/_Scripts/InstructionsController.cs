f55a7b1f6c1b9facabfa2ee894e764a6c07da123
pegasus57 <ruiiiyang@outlook.com>
2016-03-31 23:43:01 -0400

    Created Menu

---
+++ Assets/_Scripts/InstructionsController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class InstructionsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BackButtonClick()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
